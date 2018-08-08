a57f07c1794820662fe74f08db9e02458e0471a3
mca93 <39198040+mca93@users.noreply.github.com>
2018-08-09 01:36:15 +0200

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { OverdraftPage } from '../overdraft/overdraft';
+import { OverdraftService } from '../../services/overdraft.service';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+    overdrafts: {customer: string, balance: number, amount: number, overdraf: number} [] = [];
+
+  constructor(private overdraftService: OverdraftService, public navCtrl: NavController) {
+
+  }
+  ionViewWillEnter(){
+    this.overdrafts= this.overdraftService.getOverdrafts();
+  }
+  setNewOverdraft(){
+    this.navCtrl.push(OverdraftPage);
+  }
+}
